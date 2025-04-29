4915e6c6193d6e091b3b319bad55b97d2b76d793
Caio Graça Melo <caio.g.melo@hotmail.com>
2025-04-29 14:39:32 -0300

    Setting up the project to run with streamlit

--- src/helpers/excel.py
+++ src/helpers/excel.py
@@ -3,12 +3,19 @@ import typing as t
 import pandas as pd
 
 
+# Function to clean monetary values
+def clean_number_values(val):
+    if isinstance(val, str):
+        val = val.replace("€", "").replace("%", "").replace(",", "").strip()
+    return val
+
+
 def find_table_data(sheet, table_name, first_column):
     table_found = False
     columns = []
     data = []
 
-    for row in sheet:
+    for i, row in enumerate(sheet):
         if (
             isinstance(row[0], str) and table_name in row[0]
         ):  # Check if we found our table
@@ -19,6 +26,15 @@ def find_table_data(sheet, table_name, first_column):
 
         if not columns and first_column == row[0]:  # Check row with the columns
             columns = [col for col in row if col is not None]
+
+            if "Bets" in columns and "Accepted Bets" in sheet[i - 1]:
+                indexes = [i for i, x in enumerate(row) if x == "Bets"]
+                columns = (
+                    columns[: indexes[0]]
+                    + [f"Accepted {col}" for col in columns[indexes[0] : indexes[1]]]
+                    + [f"Rejected {col}" for col in columns[indexes[1] :]]
+                )
+
             continue
         elif columns and row[0]:  # Start saving table data
             data.append([val for val in row if val is not None])
@@ -28,7 +44,18 @@ def find_table_data(sheet, table_name, first_column):
     if not data:
         return None
 
-    return pd.DataFrame(data, columns=columns)
+    df = pd.DataFrame(data, columns=columns)
+
+    # Apply to all object columns
+    for col in df.select_dtypes(include="object").columns:
+        df[col] = df[col].apply(clean_number_values)
+        
+        try:
+            df[col] = pd.to_numeric(df[col])
+        except:
+            pass
+
+    return df
 
 
 def get_sheet_data(
--- src/main.py
+++ src/main.py
@@ -1,47 +1,70 @@
 from openpyxl import load_workbook
 from pathlib import Path
 from helpers import mts_sections as mts
+import pandas as pd
+import streamlit as st
 
 # Define the file path
 file_name = "20250423_SCBrasilCapixabaES_PortoVitoriaFCES_60021497"
 file_path = Path(__file__).parents[1] / "dist" / f"{file_name}.xlsx"
 
+st.set_page_config(layout="wide")
+
+st.title("Cartola FC Analytics")
+
 # Load the workbook
 wb = load_workbook(file_path, data_only=True)
 
-overall_summary_data = mts.get_overall_summary_data(wb)
-market_cluster_data = mts.get_market_cluster_data(wb)
-market_summary_data = mts.get_market_summary_data(wb)
-market_analysis_data = mts.get_market_analysis_data(wb)
-customer_summary_data = mts.get_customer_summary_data(wb)
-top_customers_data = mts.get_top_customers_data(wb)
-period_summary_data = mts.get_period_summary_data(wb)
-score_summary_data = mts.get_score_summary_data(wb)
-ccf_category_data = mts.get_ccf_category_summary_data(wb)
 
-print("\nOverall Summary")
-print(overall_summary_data)
+def set_session_state(wb, session_state_key, data_function):
+    if session_state_key not in st.session_state:
+        st.session_state[session_state_key] = data_function(wb)
 
-print("\nMarket Cluster")
-print(market_cluster_data)
+    return st.session_state[session_state_key]
 
-print("\nMarket Summary")
-print(market_summary_data)
 
-print("\nMarket Analysis")
-print(market_analysis_data)
+overall_summary_data = set_session_state(
+    wb, "overall_summary_data", mts.get_overall_summary_data
+)
+market_cluster_data = set_session_state(
+    wb, "market_cluster_data", mts.get_market_cluster_data
+)
+market_summary_data = set_session_state(
+    wb, "market_summary_data", mts.get_market_summary_data
+)
+market_analysis_data = set_session_state(
+    wb, "market_analysis_data", mts.get_market_analysis_data
+)
+customer_summary_data = set_session_state(
+    wb, "customer_summary_data", mts.get_customer_summary_data
+)
+top_customers_data = set_session_state(
+    wb, "top_customers_data", mts.get_top_customers_data
+)
+period_summary_data = set_session_state(
+    wb, "period_summary_data", mts.get_period_summary_data
+)
+score_summary_data = set_session_state(
+    wb, "score_summary_data", mts.get_score_summary_data
+)
+ccf_category_data = set_session_state(
+    wb, "ccf_category_data", mts.get_ccf_category_summary_data
+)
 
-print("\nCustomer Summary")
-print(customer_summary_data)
 
-print("\nTop Customers")
-print(top_customers_data)
+def display_data_dict(title: str, data_dict: dict[str, pd.DataFrame]):
+    st.subheader(title)
+    for key, df in data_dict.items():
+        with st.expander(f"{key}", expanded=False):
+            st.dataframe(df)
 
-print("\nPeriod Summary")
-print(period_summary_data)
 
-print("\nScore Summary")
-print(score_summary_data)
-
-print("\nCCF Category Summary")
-print(ccf_category_data)
+display_data_dict("Overall Summary", overall_summary_data)
+display_data_dict("Market Cluster", market_cluster_data)
+display_data_dict("Market Summary", market_summary_data)
+display_data_dict("Market Analysis", market_analysis_data)
+display_data_dict("Customer Summary", customer_summary_data)
+display_data_dict("Top Customers", top_customers_data)
+display_data_dict("Period Summary", period_summary_data)
+display_data_dict("Score Summary", score_summary_data)
+display_data_dict("CCF Category Summary", ccf_category_data)
