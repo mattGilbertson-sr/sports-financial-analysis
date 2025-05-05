306df0171e6b5ccd04577791e316de24bd7eb6ca
Caio Graça Melo <caio.g.melo@hotmail.com>
2025-05-05 13:26:31 -0300

    Added the 'Match %' column

--- src/helpers/mts_sections/market_analysis.py
+++ src/helpers/mts_sections/market_analysis.py
@@ -9,6 +9,14 @@ def get_market_analysis_data(workbook: Workbook):
         ("Market Analysis - Singles", "Market"),
     ]
 
-    return get_sheet_data(
+    df_dict = get_sheet_data(
         workbook=workbook, sheet_name=sheet_name, target_tables=targeted_tables
     )
+
+    dict_key = list(df_dict.keys())[0]
+
+    total_match = df_dict[dict_key]["Total T/O"].sum()
+
+    df_dict[dict_key]["Match %"] = df_dict[dict_key]["Total T/O"] / total_match
+
+    return df_dict
--- src/helpers/mts_sections/market_cluster.py
+++ src/helpers/mts_sections/market_cluster.py
@@ -9,6 +9,14 @@ def get_market_cluster_data(workbook: Workbook):
         ("Market Cluster - Singles - Accepted/Rejected", "Market"),
     ]
 
-    return get_sheet_data(
+    df_dict = get_sheet_data(
         workbook=workbook, sheet_name=sheet_name, target_tables=targeted_tables
     )
+
+    dict_key = list(df_dict.keys())[0]
+
+    total_match = df_dict[dict_key]["Total T/O"].sum()
+
+    df_dict[dict_key]["Match %"] = df_dict[dict_key]["Total T/O"] / total_match
+
+    return df_dict
