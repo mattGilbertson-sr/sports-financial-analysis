b53b87fe6fd06c2d5dee79717c2ad415c3f581aa
Caio Graça Melo <caio.g.melo@hotmail.com>
2025-05-08 11:20:12 -0300

    Improve get_market_percentage function

--- src/helpers/mts_sections/market_analysis.py
+++ src/helpers/mts_sections/market_analysis.py
@@ -5,13 +5,33 @@ from openpyxl import Workbook
 from helpers.excel import get_sheet_data
 
 
-def get_market_percentage(market: pd.Series, market_summary_df: pd.DataFrame) -> float:
-    df_filtered = market_summary_df[market_summary_df["Market"] == market["Market"]]
+def get_market_percentage(
+    market: pd.Series, market_summary_data: t.Dict[str, pd.DataFrame]
+) -> float:
+    # Filter by period
+    if "1st half" in market["Market"]:
+        df_filtered = market_summary_data["Market Summary - Singles - 1H"]
+    elif "2nd half" in market["Market"]:
+        df_filtered = market_summary_data["Market Summary - Singles - 2H"]
+    else:
+        df_filtered = market_summary_data["Market Summary - Singles - FT"]
+
+    # Filter by market
+    if "handicap" in market["Market"].lower():
+        df_filtered = df_filtered[
+            df_filtered["Market"].str.contains("handicap", case=False, na=False)
+        ]
+    elif "total" in market["Market"].lower():
+        df_filtered = df_filtered[
+            df_filtered["Market"].str.contains("total", case=False, na=False)
+        ]
+    else:
+        df_filtered = df_filtered[df_filtered["Market"] == market["Market"]]
 
     if not len(df_filtered):
         return 0
 
-    return market["Total T/O"] / df_filtered.iloc[0]["Total T/O"]
+    return market["Total T/O"] / df_filtered["Total T/O"].sum()
 
 
 def get_market_analysis_data(
@@ -28,12 +48,10 @@ def get_market_analysis_data(
         workbook=workbook, sheet_name=sheet_name, target_tables=targeted_tables
     )
 
-    market_summary_df = market_summary_data["Market Summary - Singles"]
-
     for df in df_dict.values():
         df["Match %"] = df["Total T/O"] / total_match_turnover
         df["Market %"] = df.apply(
-            lambda x: get_market_percentage(x, market_summary_df), axis=1
+            lambda x: get_market_percentage(x, market_summary_data), axis=1
         )
 
     return df_dict
