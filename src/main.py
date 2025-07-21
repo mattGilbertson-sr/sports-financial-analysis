e5e7c06a0ae95d84d7ea80d6e139548a4bf0e3d1
Caio Graça Melo <caio.g.melo@hotmail.com>
2025-07-21 15:14:44 -0300

    Remove unnecessary references in the project

--- src/main.py
+++ src/main.py
@@ -1,11 +1,11 @@
 from openpyxl import load_workbook
-from helpers import mts_sections as mts
+from helpers import sections
 import pandas as pd
 import streamlit as st
 
 from helpers.streamlit import display_data_dict, set_session_state
 
-mts_tabs = {
+tabs = {
     "Overall Summary": None,
     "Market Cluster": None,
     "Market Summary": None,
@@ -20,7 +20,7 @@ mts_tabs = {
 pd.options.mode.chained_assignment = None  # default='warn'
 
 st.set_page_config(layout="wide")
-st.title("MTS Analysis")
+st.title("Sports Financial Analysis")
 
 # Upload the file
 uploaded_file = st.file_uploader("Upload the .xlsx file", type=["xlsx"])
@@ -32,78 +32,81 @@ if uploaded_file:
     wb = load_workbook(uploaded_file, data_only=True)
 
     # Overall Summary
-    mts_tabs["Overall Summary"] = set_session_state(
-        wb, "overall_summary_data", mts.get_overall_summary_data, uploaded_file_name
+    tabs["Overall Summary"] = set_session_state(
+        wb,
+        "overall_summary_data",
+        sections.get_overall_summary_data,
+        uploaded_file_name,
     )
 
     # Get total match turnover from the Overall Summary
-    total_match_turnover = mts_tabs["Overall Summary"][
+    total_match_turnover = tabs["Overall Summary"][
         "PreMatch/Live Summary - Singles Only"
     ]["Total T/O"].sum()
 
     # Get the rest of the tabs data
-    mts_tabs["Market Cluster"] = set_session_state(
+    tabs["Market Cluster"] = set_session_state(
         wb,
         "market_cluster_data",
-        mts.get_market_cluster_data,
+        sections.get_market_cluster_data,
         uploaded_file_name,
         total_match_turnover=total_match_turnover,
     )
-    mts_tabs["Market Summary"] = set_session_state(
+    tabs["Market Summary"] = set_session_state(
         wb,
         "market_summary_data",
-        mts.get_market_summary_data,
+        sections.get_market_summary_data,
         uploaded_file_name,
         total_match_turnover=total_match_turnover,
     )
-    mts_tabs["Market Analysis"] = set_session_state(
+    tabs["Market Analysis"] = set_session_state(
         wb,
         "market_analysis_data",
-        mts.get_market_analysis_data,
+        sections.get_market_analysis_data,
         uploaded_file_name,
-        market_summary_data=mts_tabs["Market Summary"],
+        market_summary_data=tabs["Market Summary"],
         total_match_turnover=total_match_turnover,
     )
-    mts_tabs["Customer Summary"] = set_session_state(
+    tabs["Customer Summary"] = set_session_state(
         wb,
         "customer_summary_data",
-        mts.get_customer_summary_data,
+        sections.get_customer_summary_data,
         uploaded_file_name,
         total_match_turnover=total_match_turnover,
     )
-    mts_tabs["Top Customers"] = set_session_state(
+    tabs["Top Customers"] = set_session_state(
         wb,
         "top_customers_data",
-        mts.get_top_customers_data,
+        sections.get_top_customers_data,
         uploaded_file_name,
-        market_summary_data=mts_tabs["Market Summary"],
-        market_analysis_data=mts_tabs["Market Analysis"],
+        market_summary_data=tabs["Market Summary"],
+        market_analysis_data=tabs["Market Analysis"],
     )
-    mts_tabs["Period Summary"] = set_session_state(
+    tabs["Period Summary"] = set_session_state(
         wb,
         "period_summary_data",
-        mts.get_period_summary_data,
+        sections.get_period_summary_data,
         uploaded_file_name,
         total_match_turnover=total_match_turnover,
     )
-    mts_tabs["Score Summary"] = set_session_state(
+    tabs["Score Summary"] = set_session_state(
         wb,
         "score_summary_data",
-        mts.get_score_summary_data,
+        sections.get_score_summary_data,
         uploaded_file_name,
         total_match_turnover=total_match_turnover,
     )
-    mts_tabs["CCF Category Summary"] = set_session_state(
+    tabs["CCF Category Summary"] = set_session_state(
         wb,
         "ccf_category_data",
-        mts.get_ccf_category_summary_data,
+        sections.get_ccf_category_summary_data,
         uploaded_file_name,
         total_match_turnover=total_match_turnover,
     )
 
     # Only generate tabs if there is valid data for display
     streamlit_tabs = st.tabs(
-        [tab_key for tab_key, tab_data in mts_tabs.items() if tab_data is not None]
+        [tab_key for tab_key, tab_data in tabs.items() if tab_data is not None]
     )
 
     with streamlit_tabs[0]:
@@ -111,11 +114,11 @@ if uploaded_file:
 
     tabs_index_counter = 0
 
-    for mts_tab_data in mts_tabs.values():
-        if not mts_tab_data:
+    for tab_data in tabs.values():
+        if not tab_data:
             continue
 
-        display_data_dict(streamlit_tabs[tabs_index_counter], mts_tab_data)
+        display_data_dict(streamlit_tabs[tabs_index_counter], tab_data)
         tabs_index_counter += 1
 
     st.session_state.file_name = uploaded_file.name
